6873bed40383bc2792aadb65c6cb5e501ee819b6
mitrank <mitrankshah@yahoo.com>
2024-11-22 00:09:26 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from "./sections/Header.jsx";
+
+const App = () => {
+  return (
+    <main>
+      <Header/>
+    </main>
+  )
+}
+export default App
---
+++ src/sections/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <a>
+          <img src="/images/xora.svg" width={115} height={55} alt="logo" />
+        </a>
+      </div>
+    </header>
+  )
+}
+export default Header
